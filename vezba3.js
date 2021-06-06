514a949c4b1b4dc3933d1bb60bf91057e5446476
nadjaivk984 <nadjaivkovic984@gmail.com>
2021-06-06 12:24:34 +0200

    test

--- vezba3.js
+++ vezba3.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded",function(){
   
 taketext();
 textArg("Passed Text");
+buildingDrop(["Jan","Feb","March","April","May","Jun","July","Avgust"],document.getElementById("month"));
+var d=document.querySelector("div");
+//console.log(d);
+buildingDrop1(["Jan","Feb","March","April","May","Jun","July","Avgust"],d);
+var lastDiv=document.getElementsByTagName("div")[1];
+//console.log(lastDiv);
+buildingDrop1(["Jan","Feb","March","April","May","Jun","July","Avgust"],lastDiv);
+validaion();
+
 
 });
 
@@ -18,3 +27,48 @@ function textArg(a){
 
 
 }
+
+
+function buildingDrop(a,b){
+   
+    
+  for(var i=0;i<a.length;i++){
+      
+      var option=document.createElement("option");
+      var text=document.createTextNode(a[i]);
+      option.appendChild(text);
+     
+     b.appendChild(option);
+  }
+
+ 
+ 
+}  
+
+function buildingDrop1(a,b){
+    
+   
+        var select=document.createElement("select");
+        for(var i=0;i<a.length;i++){
+        var option=document.createElement("option");
+        var text=document.createTextNode(a[i]);
+       
+       
+        option.appendChild(text);
+
+    
+     select.appendChild(option);
+       b.appendChild(select);
+
+       
+
+    }   
+   
+  }  
+  function validaion(){
+      var el=document.querySelector("input");
+      if(el.hasAttribute("required")){
+          el.style.border="red 1px solid";
+
+      }
+  }
